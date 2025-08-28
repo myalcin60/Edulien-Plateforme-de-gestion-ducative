11a102b15f850baabf90c3e02ba7a75caee8de0d
M Yalcin <yalcin@gmail.com>
2025-08-28 15:15:10 +0200

    profile updated

--- src/controllers/profile_controller.php
+++ src/controllers/profile_controller.php
@@ -7,7 +7,7 @@ include_once __DIR__ . '/../../views/partiel/toast.php';
 // update profole
 if (isset($_SERVER['HTTP_REFERER']) && str_contains($_SERVER['HTTP_REFERER'], "dashboard.php") and $_SERVER['REQUEST_METHOD'] === 'POST') {
 
-    update_user_profile($_POST['id'], $_POST['email'], $_POST['first_name'], $_POST['last_name'], $_POST['gender']);
+    update_user_profile($_POST['id'], $_POST['email'], trim($_POST['first_name']), trim($_POST['last_name']), $_POST['gender']);
    
     if($_POST['id'][0]=='T'){
     header("location: ../../views/pages/teacher_dashboard.php?form=profile");
